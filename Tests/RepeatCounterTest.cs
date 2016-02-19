89301ae82e890c563b7a47a965b616c5d5176960
simon <simonctemple@live.com>
2016-02-19 09:03:59 -0800

    add initial functionality and first passing test

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -18,5 +18,19 @@ namespace RepeatCounter.Objects
       //Assert
       Assert.Equal(1, result);
     }
+    [Fact]
+    public void Test2_IfInput1WillCountInput2WithMoreThanOneWord_ReturnCount()
+    {
+      //Arrange
+      string input1 = "Hello everybody";
+      string input2 = "hello";
+      RepeatCounter testWord = new RepeatCounter(input1, input2);
+
+      //Act
+      int result = testWord.CountRepeats();
+
+      //Assert
+      Assert.Equal(1, result);
+    }
   }
 }
