f2744cc1207b41398fc167d662195ca93ef629da
simon <simonctemple@live.com>
2016-02-19 09:02:21 -0800

    add initial functionality and first passing test

---
+++ Objects/RepeatCounter.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+
+namespace RepeatCounter.Objects
+{
+  public class RepeatCounter
+  {
+    private string _input1;
+    private string _input2;
+
+    public RepeatCounter(string myInput1, string myInput2)
+    {
+      _input1 = myInput1;
+      _input2 = myInput2;
+    }
+
+
+    public int CountRepeats()
+    {
+      int repeatCount = 0;
+      string[] phraseArray = _input1.Split(' ');
+      for (var i = 0; i < phraseArray.Length; i++)
+      {
+        if(phraseArray[i].ToLower() == _input2.ToLower())
+        {
+          repeatCount += 1;
+        }
+      }
+      return repeatCount;
+    }
+
+  }
+}
---
+++ Tests/RepeatCounterTest.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Xunit;
+namespace RepeatCounter.Objects
+{
+  public class FindAndReplaceTest
+  {
+    [Fact]
+    public void Test1_IfInput1WillCountInput2LowerCase_ReturnCount()
+    {
+      //Arrange
+      string input1 = "Hello";
+      string input2 = "hello";
+      RepeatCounter testWord = new RepeatCounter(input1, input2);
+
+      //Act
+      int result = testWord.CountRepeats();
+
+      //Assert
+      Assert.Equal(1, result);
+    }
+  }
+}
